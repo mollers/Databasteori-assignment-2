727100548ddb8e9a5d7fbc67d5a2571547ecdf6d
Andreas Möller <andreasmoller94@gmail.com>
2017-12-11 16:42:16 +0100

    Changed to bufferedreader and Stringbuilder

--- Reddit Database/src/RedditDB.java
+++ Reddit Database/src/RedditDB.java
@@ -1,8 +1,9 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -24,7 +25,7 @@ public class RedditDB
 
 			statement.executeUpdate("drop table if exists comment");
 			statement.executeUpdate("create table comment (id string, parent_id string, link_id string, name string, author string, body string, subreddit_id string, subreddit string, score integer, created_utc integer)");
-			loadFile("C:\\Users\\Andreas\\OneDrive\\Documents\\Datateknik högskoleingenjör\\2DV513 Databasteori\\Assignment 2\\Reddit Database\\src\\RC_2007-10.json", statement);
+			loadFileBufferedReaderStringBuilder("C:\\Users\\Andreas\\Desktop\\databas\\RC_2011-07.json", statement);
 			//ResultSet rs = statement.executeQuery("select * from comment");
 		}
 		catch(SQLException e)
@@ -47,7 +48,7 @@ public class RedditDB
 			}
 		}
 	}
-	private static void loadFile(String path, Statement statement){
+	private static void loadFileScannerConcatenation(String path, Statement statement){
 		File file = new File(path);
 		Scanner scan;
 		try {
@@ -55,20 +56,20 @@ public class RedditDB
 			long start = System.currentTimeMillis();
 			int counter = 0;
 			while(scan.hasNextLine()){
+				System.out.println(++counter);
 				JSONObject jsObj = new JSONObject(scan.nextLine());
-																	//(id string, parent_id string, link_id string, name string, author string, body string, subreddit_id string, subreddit string, score integer, created_utc integer)
+				//(id string, parent_id string, link_id string, name string, author string, body string, subreddit_id string, subreddit string, score integer, created_utc integer)
 				String str = "insert into comment values(" + "'" + jsObj.getString("id") + "'"  + ","
 						+ "'"+ jsObj.getString("parent_id")+ "'"+ "," + "'"+ jsObj.getString("link_id")+ "'"+ ","
 						+ "'"+ jsObj.getString("name")+ "'"+ ","+ "'"+ jsObj.getString("author")+ "'"+ ","
 						+ "'"+ "body"+ "'"+ ","+ "'"+ jsObj.getString("subreddit_id")+ "'"+ ","
 						+ "'"+ jsObj.getString("subreddit")+ "'"+ ","+ jsObj.getInt("score")+ ","
-					+ jsObj.getInt("created_utc") + ")";
-				System.out.println(++counter);
+						+ jsObj.getInt("created_utc") + ")";
 				statement.executeUpdate(str);
 			}
 			scan.close();
 			long end = System.currentTimeMillis();
-			
+
 			System.out.println(end-start + " ms " + (end-start)/1000 + " s " +(end-start)/60000 + " min ");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -77,5 +78,31 @@ public class RedditDB
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	private static void loadFileBufferedReaderStringBuilder(String path, Statement statement){
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			String line;
+			long start = System.currentTimeMillis();
+			int counter = 0;
+			while((line = br.readLine())!= null){
+				System.out.println(++counter);
+				JSONObject jsObj = new JSONObject(line);
+				StringBuilder sb = new StringBuilder();
+				sb.append("insert into comment values('").append(jsObj.getString("id")).append("','").append(jsObj.getString("parent_id"))
+				.append("','").append(jsObj.getString("link_id")).append("','").append(jsObj.getString("name")).append("','")
+				.append(jsObj.getString("author")).append("','").append("body").append("','").append(jsObj.getString("subreddit_id"))
+				.append("','").append(jsObj.getString("subreddit")).append("',").append(jsObj.getInt("score")).append(",").append(jsObj.getInt("created_utc")).append(")");
+				
+				statement.executeUpdate(sb.toString());
+			}
+			br.close();
+			long end = System.currentTimeMillis();
+
+			System.out.println(end-start + " ms " + (end-start)/1000 + " s " +(end-start)/60000 + " min ");
+		}catch(Exception e){
+			System.out.println(e);
+		}
+
 	}
 }
