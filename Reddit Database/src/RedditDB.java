ef6e4e8dadf90b8ee44f6f18ef348ff481562385
Andreas Möller <andreasmoller94@gmail.com>
2017-12-13 13:00:45 +0100

    With prepared statement

--- Reddit Database/src/RedditDB.java
+++ Reddit Database/src/RedditDB.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -21,12 +23,27 @@ public class RedditDB
 			// create a database connection
 			connection = DriverManager.getConnection("jdbc:sqlite:reddit.db");
 			Statement statement = connection.createStatement();
+			
 			statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
 			statement.executeUpdate("drop table if exists comment");
 			statement.executeUpdate("create table comment (id string, parent_id string, link_id string, name string, author string, body string, subreddit_id string, subreddit string, score integer, created_utc integer)");
-			loadFileBufferedReaderStringBuilder("C:\\Users\\Andreas\\Desktop\\databas\\RC_2011-07.json", statement);
-			//ResultSet rs = statement.executeQuery("select * from comment");
+			PreparedStatement ps = connection.prepareStatement("INSERT INTO comment VALUES (?,?,?,?,?,?,?,?,?,?)");
+			loadFileBufferedReaderStringBuilder("RC_2007-10.json", ps);
+			ResultSet rs = statement.executeQuery("select * from comment limit 10");
+			while (rs.next()) {
+				System.out.println("id = " + rs.getString("id"));
+				System.out.println("name = " + rs.getString("parent_id"));
+				System.out.println("name = " + rs.getString("link_id"));
+				System.out.println("name = " + rs.getString("name"));
+				System.out.println("name = " + rs.getString("author"));
+				System.out.println("name = " + rs.getString("body"));
+				System.out.println("name = " + rs.getString("subreddit_id"));
+				System.out.println("name = " + rs.getString("subreddit"));
+				System.out.println("name = " + rs.getInt("score"));
+				System.out.println("name = " + rs.getInt("name"));
+			}
+			System.out.println("done!");
 		}
 		catch(SQLException e)
 		{
@@ -59,6 +76,8 @@ public class RedditDB
 				System.out.println(++counter);
 				JSONObject jsObj = new JSONObject(scan.nextLine());
 				//(id string, parent_id string, link_id string, name string, author string, body string, subreddit_id string, subreddit string, score integer, created_utc integer)
+				statement.executeUpdate("INSERT INTO comment VALUES ('id'");
+				
 				String str = "insert into comment values(" + "'" + jsObj.getString("id") + "'"  + ","
 						+ "'"+ jsObj.getString("parent_id")+ "'"+ "," + "'"+ jsObj.getString("link_id")+ "'"+ ","
 						+ "'"+ jsObj.getString("name")+ "'"+ ","+ "'"+ jsObj.getString("author")+ "'"+ ","
@@ -79,7 +98,7 @@ public class RedditDB
 			e.printStackTrace();
 		}
 	}
-	private static void loadFileBufferedReaderStringBuilder(String path, Statement statement){
+	private static void loadFileBufferedReaderStringBuilder(String path, PreparedStatement ps){
 		try{
 			BufferedReader br = new BufferedReader(new FileReader(path));
 			String line;
@@ -88,14 +107,27 @@ public class RedditDB
 			while((line = br.readLine())!= null){
 				System.out.println(++counter);
 				JSONObject jsObj = new JSONObject(line);
-				StringBuilder sb = new StringBuilder();
-				sb.append("insert into comment values('").append(jsObj.getString("id")).append("','").append(jsObj.getString("parent_id"))
-				.append("','").append(jsObj.getString("link_id")).append("','").append(jsObj.getString("name")).append("','")
-				.append(jsObj.getString("author")).append("','").append("body").append("','").append(jsObj.getString("subreddit_id"))
-				.append("','").append(jsObj.getString("subreddit")).append("',").append(jsObj.getInt("score")).append(",").append(jsObj.getInt("created_utc")).append(")");
-				
-				statement.executeUpdate(sb.toString());
+
+				ps.setString(1, jsObj.getString("id"));
+				ps.setString(2, jsObj.getString("parent_id"));
+				ps.setString(3, jsObj.getString("link_id"));
+				ps.setString(4, jsObj.getString("name"));
+				ps.setString(5, jsObj.getString("author"));
+				ps.setString(6, jsObj.getString("body"));
+				ps.setString(7, jsObj.getString("subreddit_id"));
+				ps.setString(8, jsObj.getString("subreddit"));
+				ps.setInt(9, jsObj.getInt("score"));
+				ps.setInt(10, jsObj.getInt("created_utc"));
+				ps.addBatch();
+				/*if((counter % 10000) == 0)
+		        {
+		            System.out.println(counter);
+		            ps.executeBatch();
+		            ps.clearBatch();
+		        }*/
 			}
+			ps.executeBatch();
+            ps.clearBatch();
 			br.close();
 			long end = System.currentTimeMillis();
 
